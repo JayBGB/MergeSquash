2aef473968ef716f86b4e75afd27d99384e91a41
Borja Garcia Barrera <bgarciabarrera@danielcastelao.org>
2023-02-23 16:12:09 +0100

    # Conflicts: #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Leader commit #1
+        // Leader commit #2
+        // Leader commit #3
         //Colab commit #1
         //Colab commit #2
         //Colab commit #3
